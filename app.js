e708879cf01d2b58a293a86387b6947258859df8
Anug Saha <anugsaha@gmail.com>
2021-04-27 15:33:15 -0400

    add welcome messege

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("World"));
+app.get("/", (req, res) => res.send("Hello World, This is MERN"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
