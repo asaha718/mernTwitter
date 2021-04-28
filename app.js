2f366d6e3db1439cb7f6f788dbbec89407b340a5
Anug Saha <anugsaha@gmail.com>
2021-04-27 21:37:29 -0400

    add routes

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World, This is MERN"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
