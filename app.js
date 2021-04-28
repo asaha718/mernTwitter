d96e1d732493965d600a77441ce0a296bf849183
Anug Saha <anugsaha@gmail.com>
2021-04-28 14:33:00 -0400

    create new user creds

--- app.js
+++ app.js
@@ -4,20 +4,31 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-
+const User = require("./models/User")
+const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World, This is MERN"));
+    
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get("/", (req, res) => { 
+    const user = new User({ 
+        handle: "anug", 
+        email: "anug@anug.com", 
+        password: "123456"
+    })
+    user.save()
+    res.send("Hello World, This is MERN")
+});
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
